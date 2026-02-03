2dc4faaa03a01c7871f287336fe2c0aaea024107
Roy <shuwj81@daum.net>
2026-02-03 17:34:04 +0900

    refator: ci  병합

--- order/src/main/java/com/popcorn/order/annotation/CheckAuth.java
+++ order/src/main/java/com/popcorn/order/annotation/CheckAuth.java
@@ -15,7 +15,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CheckAuth {
- 
+
     /**
      * 허용할 역할들
      * MANAGER, OWNER, ADMIN 등
