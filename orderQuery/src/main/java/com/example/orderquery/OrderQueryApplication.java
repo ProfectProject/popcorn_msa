aa5a3a4f967216a4ec129007aea44cb62fd06bd0
newo <kshandy0221@gmail.com>
2026-02-23 14:09:51 +0900

    fix : common 패키지 import

--- orderQuery/src/main/java/com/example/orderquery/OrderQueryApplication.java
+++ orderQuery/src/main/java/com/example/orderquery/OrderQueryApplication.java
@@ -3,12 +3,12 @@ package com.example.orderquery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import com.popcorn.common.config.TransactionManagerConfig;
-import com.popcorn.common.config.DataSourceConfig;
-import com.popcorn.common.config.JdbcTemplateConfig;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.example.orderquery",
+        "com.popcorn.common"
+})
 public class OrderQueryApplication {
 
     public static void main(String[] args) {
